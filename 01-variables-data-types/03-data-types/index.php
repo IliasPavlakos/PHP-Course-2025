1f808af65fa83d49c379744f27b65f46bb8295ff
ipavlakos <pavlakosIlias@outlook.com>
2025-02-27 20:35:13 +0200

    01-03 Data types

--- 01-variables-data-types/03-data-types/index.php
+++ 01-variables-data-types/03-data-types/index.php
@@ -12,3 +12,46 @@
 - NULL
 - Resource
 */
+
+
+
+// String
+$name = 'Ilias';
+var_dump($name);
+echo '<br>';
+
+// Integer
+$age = 26;
+var_dump($age);
+echo '<br>';
+
+// Float
+$rating = 4.5;
+var_dump($rating);
+echo '<br>';
+
+// Boolean
+$available = true;
+var_dump($available);
+echo '<br>';
+
+// Array
+$friends = [ 'John', 'Jack' ];
+var_dump($friends);
+echo '<br>';
+
+// Object
+$person = new stdClass();
+var_dump($person);
+echo '<br>';
+
+// NULL
+$car = null;
+var_dump($car);
+echo '<br>';
+
+// Resource
+$file = fopen('sample.txt', 'r');
+var_dump($file);
+echo '<br>';
+
