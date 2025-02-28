86cec30e9f094ca62882c45e482baa02b6e61a33
ipavlakos <pavlakosIlias@outlook.com>
2025-02-28 15:55:08 +0200

    03-01 If statements

--- 03-control-structures/01-if-statements/index.php
+++ 03-control-structures/01-if-statements/index.php
@@ -1,10 +1,36 @@
 <?php
 
+$age = 26;
+
 // If statement
+//if($age >= 21){
+//    echo 'You can enter the casino!';
+//}
 
 // If-Else
+//if($age >= 21){
+//    echo 'You can enter the casino!';
+//} else {
+//    echo 'You can not enter the casino!';
+//}
 
 // Nested if statement
+//if($age >= 21){
+//    echo 'You can enter the casino!';
+//    if($age >= 21){
+//        echo 'You can enter the casino!';
+//    } else {
+//        echo 'You can not enter the casino!';
+//    }
+//} else {
+//    echo 'You can not enter the casino!';
+//}
 
 // If-Else-If
+if($age >= 21){
+    echo 'You can enter the casino!';
+} else if($age >= 20) {
+    echo 'You can not enter the casino!';
+}else{
 
+}
