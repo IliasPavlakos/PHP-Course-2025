644e8ced455c4a5fff9948be7cb28eac5d79acf5
ipavlakos <pavlakosIlias@outlook.com>
2025-02-28 20:08:23 +0200

    06-02 Environment super globals - Accessing global variables

--- 06-superglobals/02-env-globals/index.php
+++ 06-superglobals/02-env-globals/index.php
@@ -6,31 +6,43 @@ putenv('DB_USER=root');
 $host = getenv('DB_HOST');
 $user = getenv('DB_USER');
 
+//var_dump(getenv());
+
+$foo = 'global var';
+function test(): void
+{
+    $foo = 'local var';
+    echo 'Global var: ' . $GLOBALS['foo'] . '<br/>';
+    echo 'Local var: ' . $foo . '<br/>';
+}
+
+test();
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>System Information</title>
-  <script src="https://cdn.tailwindcss.com"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>System Information</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="bg-gray-100">
-  <div class="container mx-auto p-8 bg-white shadow-md mt-10 rounded-lg">
+<div class="container mx-auto p-8 bg-white shadow-md mt-10 rounded-lg">
     <h1 class="text-3xl font-semibold mb-4 text-center">System Information</h1>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
 
-      <div class="bg-gray-200 p-4 rounded-lg">
-        <strong class="block mb-2">DB Host:</strong>
-          <?= $host ?>
-      </div>
-      <div class="bg-gray-200 p-4 rounded-lg">
-        <strong class="block mb-2">DB User:</strong>
-          <?= $user ?>
-      </div>
+        <div class="bg-gray-200 p-4 rounded-lg">
+            <strong class="block mb-2">DB Host:</strong>
+            <?= $host ?>
+        </div>
+        <div class="bg-gray-200 p-4 rounded-lg">
+            <strong class="block mb-2">DB User:</strong>
+            <?= $user ?>
+        </div>
 
     </div>
 </body>
