a575ad3a89896a42f224d6d92a7fe7d60a8ce792
ipavlakos <pavlakosIlias@outlook.com>
2025-02-28 13:04:50 +0200

    01-07 Math functions

--- 01-variables-data-types/07-arithmetic-operators-math-functions/index.php
+++ 01-variables-data-types/07-arithmetic-operators-math-functions/index.php
@@ -9,6 +9,56 @@
 | `/`      | Division       |
 | `%`      | Modulus        |
 */
+
+$output = 0;
+
+$num1 = 10;
+$num2 = 2;
+
+$output = $num1 + $num2;
+$output = $num1 - $num2;
+$output = $num1 * $num2;
+$output = $num1 / $num2;
+$output = $num1 % $num2;
+
+// Assignment
+$num3 = 10;
+//$num3 = $num3 + 10;
+$num3 += 10;
+
+// Build-in functions
+$output = rand();
+$output = getrandmax();
+$output = rand(1,10);
+
+// round()
+$output = round(10.41);
+$output = round(10.41, 1);
+
+$output = ceil(10.41);
+
+$output = floor(10.41);
+
+// sqrt
+$output = sqrt(10.41);
+
+// pi
+$output = pi();
+
+// abs
+$output = abs(-10.41);
+
+// max
+$output = max(1,2,3);
+$output = max([1,2,3]);
+
+// min
+$output = min(1,2,3);
+$output = min([1,2,3]);
+
+// number_format
+$output = number_format(1234567.191234, 2, '.', ',');
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,7 +78,7 @@
   </header>
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-      <!-- Output -->
+      <?= $output ?>
     </div>
   </div>
 </body>
