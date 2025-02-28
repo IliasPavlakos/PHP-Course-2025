b92f318191c5cc03c18994847f760b2ba457dda0
ipavlakos <pavlakosIlias@outlook.com>
2025-02-28 17:17:13 +0200

    04-08 Anonymous functions and closures

---
+++ 04-functions/08-anonymous-functions-closures/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$square = function ($number)
+{
+    return $number * $number;
+};
+
+$result = $square(5);
+
+//echo $result;
+
+// Closures
+function createCounter()
+{
+    $count = 0;
+
+    $counter = function () use (&$count) {
+        return ++$count;
+    };
+
+    return $counter;
+}
+
+$counter = createCounter();
+
+echo $counter() . '<br/>';
+echo $counter() . '<br/>';
+echo $counter() . '<br/>';
+echo $counter() . '<br/>';
+echo $counter() . '<br/>';
